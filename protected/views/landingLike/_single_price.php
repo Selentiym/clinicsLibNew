6554f5bc89e03815703b2d5d820acf29ba4e50f2
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2016-12-12 21:33:24 +0300

    removed two classes for the first price

--- protected/views/landingLike/_single_price.php
+++ protected/views/landingLike/_single_price.php
@@ -15,7 +15,7 @@ $oldCoeff = 1.3;
 if (!$coeff) { $coeff = 1; }
 ?>
 
-<tr <?php if ($num == 1) { echo 'class="padding-top"'; } ?> class="<?php if ($price -> getHighlight()){echo "active-price";} ?>">
+<tr  class="<?php if ($price -> getHighlight()){echo "active-price";} ?> <?php if ($num == 1) { echo "padding-top"; } ?>">
     <td class="price-name"><span><?php echo $price->text; ?></span></td>
     <td class="price-new"><?php echo round($basePrice * $coeff / 1) * 1; ?>р.</td>
     <td class="price-old"><?php echo round($basePrice * $coeff * $oldCoeff / 1) * 1; ?>р.</td>
